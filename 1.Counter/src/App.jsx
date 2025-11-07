b6ee2c3c3bc79562ff3917d14241f22906c3ec6d
kr-1199 <krkoti100@gmail.com>
2025-11-07 17:37:53 +0530

    counter is updated

--- 1.Counter/src/App.jsx
+++ 1.Counter/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+import React,{useState} from "react";
+function App(){
+  const[count,setCount]=useState(0)
+  const inc=()=>{
+    setCount(count+1)
+  }
+  const dec=()=>{
+    setCount(count-1)
+  }
+  const reset=()=>{
+    setCount(0)
+  }
+  return(
+    <div>
+      <center>
+      <h1>Count:{count}</h1>
+    <button onClick={inc}>inc +</button>
+    <button onClick={dec}>Dec -</button>
+    <button onClick={reset}>Reset</button>
+    </center></div>
   )
 }
-
-export default App
+export default App;
